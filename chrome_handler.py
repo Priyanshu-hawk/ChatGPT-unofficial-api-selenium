adc03df47e25ade2cb8df0a0e0cf5ca97300f4c9
pastetreee <92103444+pastetreee@users.noreply.github.com>
2024-04-06 23:08:52 -0500

    fix api_backend

--- chrome_handler.py
+++ chrome_handler.py
@@ -2,9 +2,22 @@ import os
 import subprocess
 import time
 import sys
+import platform
 
+def get_chrome_path():
+    os_name = platform.system().lower()
+    if os_name == "darwin":
+        return "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+    elif os_name == "windows":
+        return "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+    elif os_name == "linux":
+        return "/usr/bin/google-chrome"
+    else:
+        raise Exception(f"Unsupported OS: {os_name}")
+    
 def start_chrome():
-    subprocess.Popen(['google-chrome','--remote-debugging-port=9222','--user-data-dir=chromedata'],stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    chrome_path = chrome_path = get_chrome_path()
+    subprocess.Popen([f'{chrome_path}','--remote-debugging-port=9222','--user-data-dir=chromedata'],stdout = subprocess.PIPE, stderr = subprocess.PIPE)
 
 def kill_chrome():
     subprocess.run(['killall','chrome'])
--- testing_apis.py
+++ testing_apis.py
@@ -1,6 +1,5 @@
 from api_backend import load_chrome, start_chat_gpt, make_gpt_request, stop_chat_gpt
 
-load_chrome()
 start_chat_gpt()
 a = make_gpt_request("This just a test promt for the prosing")
 print(a)
